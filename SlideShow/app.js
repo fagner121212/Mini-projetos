9533de9443ba59eeb44c3d28d032f175cf8cf7b8
Fagner Joaquim <fagneralves.xone@gmail.com>
2022-04-23 22:57:58 -0300

    commit para não perde a rotina

---
+++ SlideShow/app.js
@@ -0,0 +1,8 @@
+var containerItems = document.getElementById('container-items')
+
+containerItems.src="img/chrono.jpg"
+
+containerItems.addEventListener("click", function(){
+    alert("Vamos adicionar uma imagen aqui")
+
+})
